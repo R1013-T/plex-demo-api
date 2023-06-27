e953008d7f2af424a671e418eeb2f1aff3abbaa8
R1013-T <nosuke1013@icloud.com>
2023-06-27 11:15:03 +0900

    add hello controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      resources :hello, only: [:index]
+
+    end
+  end
 end
