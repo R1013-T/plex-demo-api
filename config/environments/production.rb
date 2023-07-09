58e7579438641ab07e58771946c6156380af3c33
R1013-T <nosuke1013@icloud.com>
2023-07-10 06:47:25 +0900

    add config.hosts << 'plex-demo-api.onrender.com'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "plex-demo-api.onrender.com"
   config.hosts << ".onrender.com"
 
   # Code is not reloaded between requests.
